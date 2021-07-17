1b00f8e0adb95e656304d670936bee81c490b0a6
Priya2120 <82706013+Priya2120@users.noreply.github.com>
2021-07-17 14:38:48 +0530

    Add files via upload

---
+++ que 1.py
@@ -0,0 +1,17 @@
+# In the above case if the expression evaluates to true the same amount of 
+# indented statements(s) following if will be executed and if the 
+# expression evaluates to false the same amount of indented statements
+# (s) following else will be executed. See the following example. 
+# The program will print the second print statement as the value of a is 10.
+
+a=10
+if(a>10):
+    print("Value of a is greater than 10")
+else :
+    print("Value of a is 10")
+
+
+
+
+
+
